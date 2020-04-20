561e401c841de87d98a259a15dcb9e58d2dad475
Niccolo` Battezzati <nbattezzati@actiaitalia.com>
2020-04-20 19:24:58 +0200

    [Comm] first implementation of the RoverSendMsg() API and basic communication task

--- ROVER5/software/Rover5_FreeRTOS/rover_comm.c
+++ ROVER5/software/Rover5_FreeRTOS/rover_comm.c
@@ -5,23 +5,40 @@
  *      Author: nbattezzati
  */
 
-#include <stdio.h>
-#include <unistd.h> 		// for the function usleep
+#include <stdlib.h>			// for NULL definition
+#include <stdbool.h>		// for bool type
+#include <sys/types.h>		// for open() syscall
+#include <sys/stat.h>		// for open() syscall
+#include <fcntl.h>			// for open() syscall
+#include <unistd.h>			// for write() syscall
+#include <stdio.h>			// for fprintf()
 
-#include "FreeRTOS.h"
-#include "task.h"
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <queue.h>
+#include <task.h>
 
 #include "rover_task.h"
+#include "rover_comm.h"
 
-#include "io.h"										// for reading or writing to I/O
-#include "system.h"									// for the configuration and register of the QSYS system
-#include "sys/alt_irq.h"
 #include "altera_avalon_uart.h"
-#include "altera_avalon_uart_regs.h"
+
+
+
+#define MAX_QUEUE_LEN	(8)		// max number of messages in a queue
+
+
 
 ////////////////////////////
 ///                      ///
 ///   PUBLIC VARIABLES   ///
+///     (to avoid!)      ///
+////////////////////////////
+
+
+////////////////////////////
+///                      ///
+///    PRIVATE TYPES     ///
 ///                      ///
 ////////////////////////////
 
@@ -32,22 +49,8 @@
 ///                      ///
 ////////////////////////////
 
-/* Xbee variables */
-unsigned char bufferxbee[1000]={0};
-int punteroxbee = 0;
-unsigned char entradaxbee=0;
-
-/* Predefined messages */
-char* listo = "O0:U0:ROBOT LISTO \r \n";
-char* recibido = "O0:U0:COMANDO RECIBIDO \r \n";
-char* fallido = "O0:U0:COMANDO DESCONOCIDO \r \n";
-char* sensores = "O0:U0:ENC XXX XXX XXX XXX XXX XXX XXX XXX \r \n";
-
-////////////////////////////
-///                      ///
-///    PRIVATE TYPES     ///
-///                      ///
-////////////////////////////
+SemaphoreHandle_t tx_queue_mutex_ = NULL;
+QueueHandle_t tx_queue_ = NULL;
 
 
 
@@ -56,8 +59,7 @@ char* sensores = "O0:U0:ENC XXX XXX XXX XXX XXX XXX XXX XXX \r \n";
 /// FUNCTION PROTOTYPES  ///
 ///                      ///
 ////////////////////////////
-void mensaje_xbee(char *mensaje);
-static void isrxbee (void * context, alt_u32 id);
+
 
 ////////////////////////////
 ///                      ///
@@ -66,25 +68,105 @@ static void isrxbee (void * context, alt_u32 id);
 ////////////////////////////
 
 
+// FUNCTION: String2Msg()
+// Description: function to create a comm_msg_t from a string of characters
+uint8_t String2Msg(const char * str, int8_t len, comm_msg_t * msg)
+{
+	uint8_t ret_val = 0;
+	int i = 0;
+
+	// check if the output message structure is not NULL
+	if (msg != NULL) {
+		// copy bytes from the string to the message buffer
+		for (i=0; i<len && i<COMM_MAX_MSG_SIZE; ++i) {
+			msg->buf[i] = str[i];
+		}
+		// set the last carriage return
+		msg->buf[i] = '\r';
+		// set message length
+		msg->len = (len < COMM_MAX_MSG_SIZE ? len : COMM_MAX_MSG_SIZE) + 1;
+		// set the return value
+		ret_val = msg->len;
+	}
+
+	return ret_val;
+}
+
+
+// FUNCTION: RoverSendMsg()
+// Description: function to send a message over the RF channel to a specific recipient
+int8_t RoverSendMsg(uint8_t to, const comm_msg_t * msg)
+{
+	comm_msg_t tmp_msg;
+	uint8_t ret_val = 0;
+
+	// check if the queue exists
+	if (tx_queue_ == NULL) {
+		ret_val = COMM_ERR_OUT_OF_MEMORY;
+	}
+	else {
+		// lock the mutex to be sure that nobody else is using the queue
+		xSemaphoreTake(tx_queue_mutex_, portMAX_DELAY);
+		// queue the message if the queue is not full
+		if (xQueueSendToBack(tx_queue_, &tmp_msg, 0) == errQUEUE_FULL) {
+			ret_val = COMM_ERR_QUEUE_FULL;
+		}
+		// release the mutex
+		xSemaphoreGive(tx_queue_mutex_);
+	}
+
+	return ret_val;
+}
+
+// FUNCTION: RoverSendBroadcastMsg()
+// Description: function to send a message over the RF channel to all listening recipients
+int8_t RoverSendBroadcastMsg(const comm_msg_t * msg)
+{
+	// TODO RoverSendBroadcastMsg()
+	return 0;
+}
+
+
 // FUNCTION: TaskComm()
 // Description: main task of the communication module.
 void RoverTaskComm(void *pvParameters)
 {
 	TickType_t last_wakeup_time = 0;
+	int uart_fd = 0;
+	comm_msg_t tx_msg;
+
 
 	// Initialize the last_wakeup_time variable with the current time.
 	last_wakeup_time = xTaskGetTickCount();
 
-	int context_xbee;
-	alt_irq_register(XBEE_IRQ,&context_xbee,isrxbee); // install XBEE ISR
-	alt_irq_enable (XBEE_IRQ);
+	// initialize the internal resources
+	tx_queue_mutex_ = xSemaphoreCreateMutex();
+	tx_queue_ = xQueueCreate(MAX_QUEUE_LEN, sizeof(comm_msg_t));
 
+	// open the XBEE UART device
+	if ((uart_fd = open("/dev/XBEE",O_RDWR)) < 0) {
+		fprintf(stderr, "[ERR] Failed to open the XBee serial port\n");
+	}
+
+	// start the task
 	while(1)
 	{
-		printf("T_comm RUNNING\n");
+		// loop over the messages to be sent
+		while (uxQueueMessagesWaiting(tx_queue_) > 0) {
+			// get a message from the queue
+			xSemaphoreTake(tx_queue_mutex_, portMAX_DELAY);
+			xQueueReceive(tx_queue_, &tx_msg, 0);
+			xSemaphoreGive(tx_queue_mutex_);
+
+			// write the message to the UART
+			printf("[DEBUG] writing message to UART\n");
+			write(uart_fd, tx_msg.buf, tx_msg.len);
+
+			// TODO: send the command to immediately transmit the message
+		}
 
 		// Wait for the next cycle.
-		vTaskDelayUntil( &last_wakeup_time, TASK_MS_2_TICKS(1000) );
+		vTaskDelayUntil( &last_wakeup_time, TASK_MS_2_TICKS(1) );
 	}
 }
 
@@ -96,73 +178,4 @@ void RoverTaskComm(void *pvParameters)
 ///                      ///
 ////////////////////////////
 
-/*************************************************************************
-**************************************************************************
-**																		**
-** Function:    mensaje_xbee()											**
-**																		**
-** Description: Funci�n donde hace el env�o de un mensaje por medio de 	**
-** 				la tarjeta XBee											**
-** 																		**
-** Notes:       En esta funci�n se define un char *mensaje, en donde	**
-** 				mensaje es un apuntador que recorre los caracteres del 	**
-** 				mensaje que se quiere enviar.							**
-**																		**
-** Returns:     N/A														**
-**																		**
-**************************************************************************
-*************************************************************************/
-void mensaje_xbee(char *mensaje){
 
-	while(*mensaje){
-		IOWR_ALTERA_AVALON_UART_TXDATA(XBEE_BASE, *mensaje);
-		mensaje++;
-		usleep(1200); // Sleep de 2mS
-	}
-}
-
-/*
- * Interrupt Functions
- */
-
-/*************************************************************************
-**************************************************************************
-**																		**
-** Function:    isrxbee()												**
-**																		**
-** Description: Funci�n que lee lo que llega a la tarjeta XBee y llena  **
-** 				un buffer con estos datos. ISR							**
-** 																		**
-** Notes:       Cuando el buffer llega a 500 o se presiona ENTER, se 	**
-** 				llama la funci�n an�lisis() para determinar los 		**
-** 				comandos recibidos.										**
-**																		**
-** Returns:     N/A														**
-**																		**
-**************************************************************************
-*************************************************************************/
-static void isrxbee (void * context, alt_u32 id){
-
-	unsigned char test;
-	test = IORD_ALTERA_AVALON_UART_RXDATA(XBEE_BASE);
-	//IOWR_ALTERA_AVALON_UART_TXDATA(XBEE_BASE, test);
-	entradaxbee = test;
-
-	/**************************************************************************
-	** Determina se si presiona un ENTER o el buffer tiene un valor superior **
-	** a 500 posiciones de memoria, analiza el buffer y reinicia el puntero  **
-	**************************************************************************/
-	if (test == 13 || punteroxbee == 500){ // En ASCII 13 equivale a ENTER
-		//analisis();
-		//analiso lo que me llego !!
-		punteroxbee = 0;
-	}
-	/**********************************************************************
-	** Llena el buffer de acuerdo a los valores que se est�n recibiendo  **
-	** en la tarjeta XBee												 **
-	**********************************************************************/
-	else{
-		bufferxbee[punteroxbee] = test;
-		punteroxbee++;
-	}
-}
--- ROVER5/software/Rover5_FreeRTOS/rover_comm.h
+++ ROVER5/software/Rover5_FreeRTOS/rover_comm.h
@@ -9,16 +9,43 @@
 
 #ifndef ROVER_COMM_H_
 #define ROVER_COMM_H_
-/*
+
+
+#define COMM_ERR_OUT_OF_MEMORY		(-1)
+#define COMM_ERR_QUEUE_FULL			(-2)
+
+
+#define COMM_MAX_MSG_SIZE			(255)	// max number of bytes in a message
+
+
+
 typedef struct {
-	uint16_t size;
-	uint8_t buf[256];
-} rover_msg_t;
+	uint8_t len;
+	uint8_t buf[COMM_MAX_MSG_SIZE];
+} comm_msg_t;
 
-void RoverSendMsg(rover_msg_t msg);
-*/
+// FUNCTION: String2Msg()
+// Description: function to create a comm_msg_t from a string of characters
+// Parameters:  - str: string containing the message to send
+//              - len: length of the string (max=COMM_MAX_MSG_SIZE chars. Characters in excess will be truncated)
+//              - msg: pointer to a comm_msg_t that will be filled with the message
+// Return:      number of characters copied into the message. If any error occurs return 0
+uint8_t String2Msg(const char * str, int8_t len, comm_msg_t * msg);
 
-// FUNCTION: TaskComm()
+// FUNCTION: RoverSendMsg()
+// Description: function to send a message over the RF channel to a specific recipient
+// Parameters:  - to: recipient address
+//              - msg: the message to be sent
+// Return:      0 on successful queuing of the message, < 0 if failed
+int8_t RoverSendMsg(uint8_t to, const comm_msg_t * msg);
+
+// FUNCTION: RoverSendBroadcastMsg()
+// Description: function to send a message over the RF channel to all listening recipients
+// Parameters:  - msg: the message to be sent
+// Return:      0 on successful queuing of the message, < 0 if failed
+int8_t RoverSendBroadcastMsg(const comm_msg_t * msg);
+
+// FUNCTION: RoverTaskComm()
 // Description: main task of the communication module.
 //              It contains the main loop that checks if new messages have arrived
 // Parameters:  - pvParameters: pointer to task parameters (pass NULL, not used)
