551b1079178332e5d8ee1cdc546b817de12ad669
Niccolo` Battezzati <nbattezzati@actiaitalia.com>
2020-07-05 17:13:24 +0200

    fix types for predefined messages functions

--- ROVER5/software/Rover5_FreeRTOS/rover_comm_msgs.h
+++ ROVER5/software/Rover5_FreeRTOS/rover_comm_msgs.h
@@ -9,9 +9,9 @@
 #define ROVER_COMM_MSGS_H_
 
 
-int32_t RoverSendMsg_MOTOR_TELEM(uint8_t from, uint8_t to, int32_t v1, int32_t v2, int32_t c1, int32_t c2);
+int8_t RoverSendMsg_MOTOR_TELEM(uint8_t from, uint8_t to, int32_t v1, int32_t v2, int32_t c1, int32_t c2);
 
-int32_t RoverSendMsg_CONTROL_OUTPUT(uint8_t from, uint8_t to, int32_t x, int32_t y, float t, int32_t lv, float av, int32_t xe, int32_t ye, float te);
+int8_t RoverSendMsg_CONTROL_OUTPUT(uint8_t from, uint8_t to, int32_t x, int32_t y, float t, int32_t lv, float av, int32_t xe, int32_t ye, float te);
 
 
 
